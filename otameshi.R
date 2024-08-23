93fc12cc2cd6982f87aca6e8b93e0d5222f1ef01
LNOKB <uminosobanoie@gmail.com>
2024-08-23 16:27:29 +0900

    kk

--- otameshi.R
+++ otameshi.R
@@ -1,34 +1,88 @@
+library(tidyverse)#前処理に必要
+library(magrittr) #パイプ演算子(%>%)を使うために必要
+
+dat <- read.table("subdata.csv", header=T,sep=",") # 生データ
+
+result = dat %>%             
+  filter(colorcond == 1 | colorcond == 2) %>%  # 中心課題正答試行のみ抽出
+  select(subnum, exposure, maskwidth, colorcond, TorF1, colorres, TorF2, mix_mix, mix_color, color_mix, color_color) %>%    # 分散分析に必要な変数の列だけを抽出して
+  group_by(subnum, exposure, colorcond, maskwidth) %>%                # グループ化で並び替えて
+  summarize(CR= sum(mix_mix), FA= sum(mix_color), miss= sum(color_mix), hit= sum(color_color),) %>%       # 要約統計（ここでは平均）を計算
+  print()  
+
+#write.csv(result, "otameshi.csv")
+
 sub <- 1
 dat1 <- read.table("hit.csv", header=F,sep=",") 
 dat2 <- read.table("miss.csv", header=F,sep=",") 
 dat3 <- read.table("correctrejection.csv", header=F,sep=",") 
 dat4 <- read.table("fa.csv", header=F,sep=",") 
-hit<-dat1[sub+1,2:16]
-miss<-dat2[sub+1,2:16]
+hit<-dat1[sub,2:16]
+miss<-dat2[sub,2:16]
 correctrejection<-dat3[sub+1,2:16]
 fa<-dat4[sub+1,2:16]
-# \response frequency vectors 
-
+kk
 
 # add_constant = TRUE adds a small value to the response frequency vectors.
 ### Function for model fitting
-fit_uvsdt_mle <- function(hit, miss, correctrejection, fa, add_constant = FALSE) {
+fit_uvsdt_mle <- function(hit, miss, correctrejection, fa, add_constant = TRUE) {
   
   # correction against extreme estimates 
   if (add_constant) {
-    hit <- hit + (1 / length(hit))
-    miss <- miss + (1 / length(miss))
-    correctrejection <- miss + (1 / length(correctrejection))
-    fa <- fa + (1 / length(fa))
+    hit <- hit + 0.001
+    miss <- miss + 0.001
+    correctrejection <- correctrejection + 0.001
+    fa <- fa + 0.001
   }
 
+  sigma83ms <- 1
+
+  sigma117ms <- sigma150ms * a
+  sigma150ms <- sigma150ms * b
+  
+  mu83ms_9deg <- 1
+  
+  mu83ms_13deg <- mu83ms_9deg * c
+  mu83ms_17deg <- mu83ms_9deg * d   
+  mu83ms_21deg <- mu83ms_9deg * e
+  mu83ms_25deg <- mu83ms_9deg * f
+  
+  mu117ms_9deg <- mu83ms_9deg * g 
+  mu117ms_13deg <- mu83ms_9deg * h
+  mu117ms_17deg <- mu83ms_9deg * i 
+  mu117ms_21deg <- mu83ms_9deg * j 
+  mu117ms_25deg <- mu83ms_9deg * k
+  
+  mu150ms_9deg <- mu83ms_9deg * l
+  mu150ms_13deg <- mu83ms_9deg * m
+  mu150ms_17deg <- mu83ms_9deg * n
+  mu150ms_21deg <- mu83ms_9deg * o
+  mu150ms_25deg <- mu83ms_9deg * p
   
   # initial guess for parameter values
-  mu <- 0
-  sigma <- 1.5
-  cri <-  0
-  cri <-   0
-  guess <- c(mu, sigma, cri)
+  
+
+  a <- 1
+  b <- 1
+  
+  c <- 1
+  d <- 1
+  e <- 1
+  f <- 1
+  g <- 1
+  h <- 1
+  i <- 1
+  j <- 1
+  k <- 1
+  l <- 1
+  m <- 1
+  n <- 1
+  o <- 1
+  p <- 1
+  
+  cri <- 0 #β
+  
+  guess <- c(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, cri)
   
   # model fit
   fit <- suppressWarnings(optim(uvsdt_logL, 
@@ -37,16 +91,55 @@ fit_uvsdt_mle <- function(hit, miss, correctrejection, fa, add_constant = FALSE)
                                 gr = NULL, method = "BFGS", control = list("maxit" = 10000)))
   
   # outputs
-  mu <-    fit$par[1]
-  sigma <- fit$par[2]
-  da <-    mu / sqrt((1 + sigma^2) / 2)
+  sigma117ms <- fit$par[1] * sigma83ms
+  sigma150ms <- fit$par[2] * sigma83ms
+  
+  mu83ms_13deg <- fit$par[3] * mu83ms_9deg
+  mu83ms_17deg <- fit$par[4] * mu83ms_9deg
+  mu83ms_21deg <- fit$par[5] * mu83ms_9deg
+  mu83ms_25deg <- fit$par[6] * mu83ms_9deg
+  
+  mu117ms_9deg <-  fit$par[7] * mu83ms_9deg
+  mu117ms_13deg <- fit$par[8] * mu83ms_9deg
+  mu117ms_17deg <- fit$par[9] * mu83ms_9deg
+  mu117ms_21deg <- fit$par[10] * mu83ms_9deg
+  mu117ms_25deg <- fit$par[11] * mu83ms_9deg
+  
+  mu150ms_9deg <-  fit$par[12] * mu83ms_9deg
+  mu150ms_13deg <- fit$par[13] * mu83ms_9deg
+  mu150ms_17deg <- fit$par[14] * mu83ms_9deg
+  mu150ms_21deg <- fit$par[15] * mu83ms_9deg
+  mu150ms_25deg <- fit$par[16] * mu83ms_9deg
+  
+    #da <-    mu / sqrt((1 + sigma^2) / 2)
   logL <- -fit$value
-  #########?############
+  
+  #########?###############################################
   cri <- data.frame(matrix(vector(), 0, 2 * n_ratings - 1))
   for (i in 1:(2 * n_ratings - 1)) {
     cri[1, i] <- fit$par[2 * n_ratings + 2 - i]
   }
-  est <- data.frame(mu = mu, sigma = sigma, da = da, cri = cri, logL = logL)
+  #########################################################
+  
+  est <- data.frame(sigma117ms = sigma117ms,
+                    sigma150ms = sigma150ms,
+                    mu83ms_9deg   = mu83ms_9deg, 
+                    mu83ms_13deg  = mu83ms_13deg, 
+                    mu83ms_17deg  = mu83ms_17deg, 
+                    mu83ms_21deg  = mu83ms_21deg, 
+                    mu83ms_25deg  = mu83ms_25deg, 
+                    mu117ms_9deg  = mu117ms_9deg, 
+                    mu117ms_13deg = mu117ms_13deg, 
+                    mu117ms_17deg = mu117ms_17deg, 
+                    mu117ms_21deg = mu117ms_21deg, 
+                    mu117ms_25deg = mu117ms_25deg, 
+                    mu150ms_9deg  = mu150ms_9deg, 
+                    mu150ms_13deg = mu150ms_13deg, 
+                    mu150ms_17deg = mu150ms_17deg,
+                    mu150ms_21deg = mu150ms_21deg, 
+                    mu150ms_25deg = mu150ms_25deg,  
+                    cri = cri, 
+                    logL = logL)
   return(est)
   
 }
@@ -56,10 +149,9 @@ fit_uvsdt_mle <- function(hit, miss, correctrejection, fa, add_constant = FALSE)
 uvsdt_logL <- function(x, inputs) {
   
   # target parameters
-  mu <-    x[1]　#【シミュレーション】これが刺激に応じた値になるように
+  mu <-    x[1]　
   sigma <- x[2]
-  cri <-   x[3:(2 * inputs$n_ratings + 1)]　#【シミュレーション】ここをいくつかの値（5水準とか？）に変えて推定の際のパラメータのあたりをつける
-  #【シミュレーション】今回は分散が条件間で違うようにする。たとえば、最も短い呈示時間のときのS1の平均μが０、標準偏差σが1とおく。そのほかの条件のμやσが刺激の関数で決まるようにする。
+  cri <-   x[3:(2 * inputs$n_ratings + 1)]　
   #【シミュレーション】今回は確信度をとっていないのでROC曲線はかけない
   
   # empirical data
@@ -71,9 +163,14 @@ uvsdt_logL <- function(x, inputs) {
   #反応率　criterionより上側の面積を出す
   pred_far <- c(0, pnorm(0 - cri, 0, 1),            1)#s1分布
   pred_hr <-  c(0, pnorm((mu - cri) / sigma, 0, 1), 1)#S2分布なのでsigmaで割る
+  #qnormは引数に確率を受け、確率密度を返す。例）累積確率が95%になる確率密度の値(1.6448)を返す
+  #pnormは引数に確率密度を受て、確率を返す。例）確率密度がマイナス無限大から1.6448までの累積確率(94.9%)を返す
+  #これだとなぜhit とfalse alarmで良い？
+  
   #反応数
   pred_nr_s1 <- sum(nr_s1) * diff(pred_far)
   pred_nr_s2 <- sum(nr_s2) * diff(pred_hr)
+  #diffは連続する要素間の差を計算。例 x <- c(5,3,4) diff(x) = (-2  1)
   
   # log likelihood
   logL <- sum(nr_s1 * log(pred_nr_s1 / sum(nr_s1)) + nr_s2 * log(pred_nr_s2 / sum(nr_s2)))#対数にしたので、S1刺激に対する対数尤度とS2刺激に対する対数尤度の単純な和をとればＯＫ
